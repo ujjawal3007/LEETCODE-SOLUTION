ceafb4271103b298a264e060a320661067bb1cc4
ujjawal3007 <ujjawalrajput3007@gmail.com>
2026-09-14 12:29:55 -0400

    Time: 0 ms (100%), Space: 42.1 MB (14.78%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        int freq = 1 , ans = nums[0];
+        int n = nums.size();
+        for(int i = 1; i < n; i++) {
+            if(nums[i] == nums[i -1]) {
+                freq++;
+            }
+            else{
+                freq = 1;
+                ans = nums[i];
+            }
+            if(freq > n/2) {
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
