93db2d33ada05706880dc5549776c2516c5d1ada
ujjawal3007 <ujjawalrajput3007@gmail.com>
2026-09-24 13:52:48 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.78%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,21 +3,21 @@ public:
     double myPow(double x, int n) {
         long binForm = n;
         if(n == 0) return 1.0;
-        if(x ==0) return 0.0;
+        if(x == 0) return 0.0;
         if(x == 1) return 1.0;
-        if(x == -1 && n % 2 == 0) return 1.0;
+        if(x == 0 && n % 2== 0) return 1.0;
         if(x == -1 && n % 2 != 0) return -1.0;
+        double ans = 1;
         if(binForm < 0) {
             x = 1/x;
             binForm = -binForm;
         }
-        double ans = 1;
         while(binForm > 0) {
             if(binForm % 2 == 1) {
                 ans *= x;
             }
-                x *= x;
-                binForm /= 2;
+            x = x * x;
+            binForm /= 2;
         }
         return ans;
     }
