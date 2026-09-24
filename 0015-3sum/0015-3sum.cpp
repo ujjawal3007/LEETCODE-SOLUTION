b2162671505f1b64919d8f1dd17810f4c4cbf80b
ujjawal3007 <ujjawalrajput3007@gmail.com>
2026-09-24 16:15:49 +0530

    Time: 39 ms (94.56%), Space: 28.9 MB (99.52%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -6,21 +6,21 @@ public:
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n; i++) {
             if(i > 0 && nums[i] == nums[i - 1]) continue;
-            int j = i + 1;
-            int k  = n - 1;
-            while( j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum < 0) {
-                    j++;
-                }
-                else if(sum > 0) {
-                    k--;
-                }
-                else{
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    j++, k--;
-                    while(j < k && nums[j] == nums[j - 1]) j++;
-                }
+              int  j = i + 1; 
+                int k = n - 1;
+                while(j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(sum < 0) {
+                        j++;
+                    }
+                    else if(sum > 0) {
+                        k--;
+                    }
+                    else{
+                        ans.push_back({nums[i], nums[j], nums[k]});
+                        j++, k--;
+                        while(j < k && nums[j] == nums[j - 1]) j++;
+                    }
             }
         }
         return ans;
