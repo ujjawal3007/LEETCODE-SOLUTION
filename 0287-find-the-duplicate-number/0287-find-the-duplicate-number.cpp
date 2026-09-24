611983500af5c6029ae8cccfcb8b0c417b50819d
ujjawal3007 <ujjawalrajput3007@gmail.com>
2026-09-24 22:14:23 +0530

    Time: 2 ms (71.89%), Space: 65 MB (69.71%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -7,13 +7,13 @@ public:
             slow = nums[slow];
             fast = nums[nums[fast]];
         }
-        while(slow != fast);
-
-        slow = nums[0];
-         while(slow != fast) {
+        while(slow != fast); {
+            slow = nums[0];
+        }
+        while(slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
-         }
-         return slow;
+        }
+        return slow;
     }
 };
