950ad643e7a49708f2f065be4e2642eec5b685fd
ujjawal3007 <ujjawalrajput3007@gmail.com>
2026-09-25 15:49:43 +0530

    Time: 0 ms (100%), Space: 26.3 MB (8.7%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,19 +3,16 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        int st = 0;
-        int end = n - 1;
+        int st = 0, end = nums.size() - 1;
         while(st <= end) {
-           int mid = st + (end - st) / 2;
+            int mid = st + (end - st) / 2;
             if(mid == 0 && nums[0] != nums[1]) return nums[mid];
-            if(mid == n - 1 && nums[n - 1] != nums[n - 2]) return nums[mid]; 
-            if(mid > 0 && mid < n-1) {
-            if(nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) {
-                return nums[mid];
-            }
-            }
+            if(mid == n - 1 && nums[n - 1] != nums[n - 2]) return nums[mid];
+             
+            if(nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) return nums[mid];
+
             if(mid % 2 == 0) {
-                if( nums[mid - 1]  == nums[mid]) {
+                if(nums[mid - 1] == nums[mid]) {
                     end = mid - 1;
                 }
                 else{
@@ -23,15 +20,14 @@ public:
                 }
             }
             else{
-                if( nums[mid - 1] == nums[mid]) {
+                if(nums[mid - 1] == nums[mid]) {
                     st = mid + 1;
-                    
                 }
                 else{
                     end = mid - 1;
+                }
             }
         }
-    }
         return -1;
     }
 };
